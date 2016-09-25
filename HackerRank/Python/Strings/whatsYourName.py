5398f437117f18eab8ab87bc06eba5760b12b98e
Jason <crg.jason@gmail.com>
2016-09-25 00:57:29 -0400

    Easy
    For some reason, commiting the easiest challenge.

---
+++ HackerRank/Python/Strings/whatsYourName.py
@@ -0,0 +1,8 @@
+#You are given the firstname and lastname of a person on two different lines. 
+#Your task is to read them and print the following:
+
+#    Hello firstname lastname! You just delved into python.
+
+fname = raw_input('')
+lname = raw_input('')
+print('Hello ' + fname + ' ' + lname + '! You just delved into python.')
